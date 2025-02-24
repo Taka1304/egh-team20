2b2cfd7757c0364c61d53af53a66324f7eb9017d
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2025-02-24 13:37:04 +0900

    fix: apiのruntime定義をnodejsに差し替え

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,17 +1,17 @@
-import { Hono } from "hono"
-import { handle } from "hono/vercel"
-import sample from "./sample"
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import sample from "./sample";
 
-export const runtime = "edge"
+export const runtime = "nodejs";
 
-const app = new Hono().basePath("/api")
-const sampleRoute = app.route("/sample", sample)
+const app = new Hono().basePath("/api");
+const sampleRoute = app.route("/sample", sample);
 
-export type AppType = typeof sampleRoute
+export type AppType = typeof sampleRoute;
 
-const GET = handle(app)
-const POST = handle(app)
-const PUT = handle(app)
-const DELETE = handle(app)
-const PATCH = handle(app)
-export { GET, POST, PUT, DELETE, PATCH }
+const GET = handle(app);
+const POST = handle(app);
+const PUT = handle(app);
+const DELETE = handle(app);
+const PATCH = handle(app);
+export { GET, POST, PUT, DELETE, PATCH };
