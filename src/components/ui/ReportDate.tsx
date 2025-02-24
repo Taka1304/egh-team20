fc095371acfb41a0df1cbfbc2cbf443b91ae40b9
ko-tarou <tkota6174@gmail.com>
2025-02-24 19:48:06 +0900

    fix dayjsを使うように変更

--- src/components/ui/ReportDate.tsx
+++ src/components/ui/ReportDate.tsx
@@ -1,24 +1,28 @@
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+
+dayjs.locale("ja");
+
 type ReportDateProps = {
   createdAt: string;
 };
 
 export function ReportDate({ createdAt }: ReportDateProps) {
-  const dateObj = new Date(createdAt);
-  const year = dateObj.getFullYear();
-  const month = dateObj.getMonth() + 1;
-  const day = dateObj.getDate();
-  const weekdays = ["日", "月", "火", "水", "木", "金", "土"];
-  const weekday = weekdays[dateObj.getDay()];
+  const dateObj = dayjs(createdAt);
+  const formattedYear = dateObj.format("YYYY");
+  const formattedMonth = dateObj.format("M");
+  const formattedDay = dateObj.format("D");
+  const formattedWeekday = dateObj.format("ddd");
 
   return (
     <div className="flex flex-col items-end">
-      <p className="text-sm text-muted-foreground self-start">{year}</p>
+      <p className="text-sm text-muted-foreground self-start">{formattedYear}</p>
       <div className="flex items-center space-x-2">
         <p className="text-4xl font-bold text-card-foreground">
-          {month}.{day}
+          {formattedMonth}.{formattedDay}
         </p>
         <div className="border border-card-foreground rounded-md px-2 py-0.5 text-sm text-card-foreground">
-          {weekday}
+          {formattedWeekday}
         </div>
       </div>
     </div>
