f904d46435c4f91403a48b574a62831f19bbf140
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2025-02-25 13:01:29 +0900

    🗃️ Badge関連の追加、DailyReportにtitle追加, formatを任意に変更

---
+++ prisma/migrations/20250225035936_badge/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - Added the required column `title` to the `DailyReport` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "DailyReport" ADD COLUMN     "title" TEXT NOT NULL,
+ALTER COLUMN "formatId" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Badge" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Badge_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserBadge" (
+    "userId" TEXT NOT NULL,
+    "badgeId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserBadge_pkey" PRIMARY KEY ("userId","badgeId")
+);
+
+-- AddForeignKey
+ALTER TABLE "UserBadge" ADD CONSTRAINT "UserBadge_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserBadge" ADD CONSTRAINT "UserBadge_badgeId_fkey" FOREIGN KEY ("badgeId") REFERENCES "Badge"("id") ON DELETE CASCADE ON UPDATE CASCADE;
