2d29171091cf6d687c57fea90b131e78bfb3be3b
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2025-02-25 13:21:52 +0900

    ✨ 日報投稿API実装

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,16 +3,19 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import users from "./users";
 import sample from "./sample";
+import reports from "./reports";
 import assets from "./assets";
 
 export const runtime = "nodejs";
 
-const app = new Hono().basePath("/api");
-const sampleRoute = app.route("/sample", sample);
-const usersRoute = app.route("/users", users);
-const assetsRoute = app.route("/assets", assets);
+const app = new Hono()
+  .basePath("/api")
+  .route("/assets", assets)
+  .route("/sample", sample)
+  .route("/reports", reports)
+  .route("/users", users)
 
-export type AppType = typeof sampleRoute | typeof assetsRoute | typeof usersRoute;
+export type AppType = typeof app;
 
 const handlers = handle(app);
 
