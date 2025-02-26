d2a9b5d000f5386cb1813bc1307969b2fa6d85d3
ko-tarou <tkota6174@gmail.com>
2025-02-26 14:06:45 +0900

    add 横幅を広げた

--- src/app/_features/TopPage/TopPage.tsx
+++ src/app/_features/TopPage/TopPage.tsx
@@ -10,7 +10,7 @@ export default function TopPage() {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full h-full">
         {/* 中央のタイムライン（スクロール可 / スクロールバー非表示） */}
         <div className="w-full flex justify-center h-full overflow-y-auto hidden-scrollbar md:col-span-2">
-          <div className="w-full max-w-2xl">
+          <div className="w-full ">
             <Timeline />
           </div>
         </div>
