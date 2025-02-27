61be1a9f89b24078434f63492444225d34c11a42
ko-tarou <tkota6174@gmail.com>
2025-02-27 15:02:04 +0900

    fix sandbox消去

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import Header from "@/app/_features/Navigate/Header/Header";
-import { Timeline } from "@/app/_features/Timeline/Timeline";
 
 export default function page() {
   return (
     <>
       <Header />
-      <Timeline />
     </>
   );
 }
