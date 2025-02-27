165dcb426d9eb420a407bae280a3240112a0d0d4
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2025-02-27 14:57:50 +0900

    fix: カスタムフックスを用いてデータ取得

--- src/app/_features/NotificationModal/NotificationModal.tsx
+++ src/app/_features/NotificationModal/NotificationModal.tsx
@@ -5,41 +5,11 @@ import { Bell } from "lucide-react";
 import { useState } from "react";
 
 import { NotificationModalView } from "@/app/_features/NotificationModal/NotificationModalView"; // 内部モジュール
-
-export type Notification = {
-  id: number;
-  user: {
-    name: string;
-    avatar: string;
-  };
-  message: string;
-  timestamp: string;
-};
-
-// **📌 ダミーデータ**
-const dummyNotifications: Notification[] = [
-  {
-    id: 1,
-    user: { name: "こーが", avatar: "/user1.jpg" },
-    message: "あなたのツイートをお気に入りに登録しました。",
-    timestamp: "2月22日",
-  },
-  {
-    id: 2,
-    user: { name: "うみさん", avatar: "/user2.jpg" },
-    message: "あなたのツイートをリツイートしました。",
-    timestamp: "2月21日",
-  },
-  {
-    id: 3,
-    user: { name: "のーとりあす", avatar: "/user3.jpg" },
-    message: "あなたをフォローしました。",
-    timestamp: "2月20日",
-  },
-];
+import { useNotifications } from "@/app/_features/NotificationModal/useNotifications";
 
 export function NotificationModal() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { notifications, isLoading } = useNotifications();
   const handleOpen = () => setIsOpen(true);
   const handleClose = () => setIsOpen(false);
 
@@ -54,7 +24,7 @@ export function NotificationModal() {
         <Bell className="h-5 w-5" />
       </Button>
 
-      {isOpen && <NotificationModalView notifications={dummyNotifications} onClose={handleClose} />}
+      {isOpen && !isLoading && <NotificationModalView notifications={notifications} onClose={handleClose} />}
     </>
   );
 }
