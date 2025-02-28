e76f931742417842816e8e4c464dcf55212f25e1
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2025-03-01 05:23:39 +0900

    fix: 初期値でDEFAULT_THEMEを与える

--- src/app/_features/ThemeSwitcher/ThemeProvider.tsx
+++ src/app/_features/ThemeSwitcher/ThemeProvider.tsx
@@ -5,16 +5,29 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes";
 import { useEffect, useState } from "react";
 
+const DEFAULT_THEME = "primary";
+
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const [selectedTheme] = useAtom(selectedThemeAtom);
+  const [selectedTheme, setSelectedTheme] = useAtom(selectedThemeAtom);
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
-    if (selectedTheme) {
-      document.documentElement.className = selectedTheme;
+
+    if (!selectedTheme) {
+      setSelectedTheme(DEFAULT_THEME);
     }
+    const themeToApply = selectedTheme || DEFAULT_THEME;
+    document.documentElement.className = themeToApply;
+
     return () => setMounted(false);
-  }, [selectedTheme]);
-  return mounted && <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  }, [selectedTheme, setSelectedTheme]);
+
+  return (
+    mounted && (
+      <NextThemesProvider {...props} defaultTheme={selectedTheme || DEFAULT_THEME}>
+        {children}
+      </NextThemesProvider>
+    )
+  );
 }
