a1c1836bb73de60cd4fee94728f99332937cf32f
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2025-03-01 02:07:39 +0900

    fix: おすすめ記事のレスポンシブ対応

--- src/app/_features/RecommendedArticles/RecommendedArticlesCard.tsx
+++ src/app/_features/RecommendedArticles/RecommendedArticlesCard.tsx
@@ -11,10 +11,10 @@ type RecommendedArticleCardProps = {
 
 export function RecommendedArticlesCard({ article }: RecommendedArticleCardProps) {
   return (
-    <motion.div whileHover={{ scale: 0.99 }} whileTap={{ scale: 0.98 }}>
-      <Link href={article.url} passHref>
-        <Card className="overflow-hidden cursor-pointer h-36 max-w-[600px] flex">
-          <div className="relative w-full min-w-44 max-w-44">
+    <motion.div whileHover={{ scale: 0.99 }} whileTap={{ scale: 0.98 }} className="w-full">
+      <Link href={article.url} passHref className="w-full">
+        <Card className="overflow-hidden cursor-pointer h-auto sm:h-36 w-full flex flex-col sm:flex-row">
+          <div className="relative w-full h-40 sm:h-full sm:w-1/3 sm:min-w-[120px] sm:max-w-[180px]">
             <Image
               src={article.thumbnail || ""}
               alt={article.title || "記事のサムネイル"}
