5304ca00c67f2ff0617232e09b9d7eb4e2ba687c
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2025-02-28 11:36:54 +0900

    fix: ローディングアニメーションを追加

--- src/app/_features/Profile/ProfileRecommendedUsers/ProfileRecommendedUsers.tsx
+++ src/app/_features/Profile/ProfileRecommendedUsers/ProfileRecommendedUsers.tsx
@@ -1,17 +1,27 @@
 "use client";
 
+import Loading from "@/app/Loading";
 import { ProfileRecommendedUserCard } from "@/app/_features/Profile/ProfileRecommendedUsers/ProfileRecommendedUsersCard/ProfileRecommendedUserCard";
 import { useRecommendUser } from "@/app/hooks/useRecommendUser";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function ProfileRecommendedUsers() {
   const { recommendedUsers, isLoading, error, followUser, unfollowUser } = useRecommendUser();
 
-  /* Todo ローディングアニメーション */
   if (isLoading) {
     return (
       <div className="p-4 border border-primary-foreground bg-card rounded-[var(--radius)] w-full">
         <h2 className="text-lg font-bold text-card-foreground mb-3">おすすめの人</h2>
-        <p className="text-muted-foreground">ローディング中...</p>
+        <div className="flex space-x-3 overflow-x-auto pb-2 custom-scrollbar">
+          {[1, 2, 3, 4, 5].map((index) => (
+            <Skeleton
+              key={index}
+              className="relative h-[196px] w-48 bg-primary-foreground rounded-lg shadow-md border overflow-hidden flex-shrink-0 flex flex-col items-center justify-center"
+            >
+              <Loading />
+            </Skeleton>
+          ))}
+        </div>
       </div>
     );
   }
